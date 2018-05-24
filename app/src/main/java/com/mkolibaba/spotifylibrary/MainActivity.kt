0f06f335301d0626b77ab283eb4a4d43ed63d136
mkolibaba <avierdaner@gmail.com>
2018-05-24 08:23:34 +0300

    Add toolbar and saving to external storage

--- app/src/main/java/com/mkolibaba/spotifylibrary/MainActivity.kt
+++ app/src/main/java/com/mkolibaba/spotifylibrary/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.mkolibaba.spotifylibrary
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.Button
 import android.widget.ListView
 import com.mkolibaba.spotifylibrary.adapter.SpotifyAlbumAdapter
-import com.mkolibaba.spotifylibrary.database.AlbumDatabase
-import com.mkolibaba.spotifylibrary.database.entity.AlbumEntity
 import com.mkolibaba.spotifylibrary.model.AlbumModel
 import com.mkolibaba.spotifylibrary.service.AlbumDatabaseService
 import com.mkolibaba.spotifylibrary.service.SpotifyService
+import com.mkolibaba.spotifylibrary.util.Util
 import com.spotify.sdk.android.authentication.AuthenticationClient
 import com.spotify.sdk.android.authentication.AuthenticationRequest
 import com.spotify.sdk.android.authentication.AuthenticationResponse
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.runBlocking
 
 class MainActivity : Activity() {
     private val CLIENT_ID = "9e81ca984c35810c96ab86e5b491"
@@ -27,57 +27,54 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),1);
+
         albumService = AlbumDatabaseService(this)
 
-        loginSpotify()
+        val syncButton = findViewById(R.id.syncButton) as Button
+        syncButton.setOnClickListener { sync() }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
         super.onActivityResult(requestCode, resultCode, intent)
 
-        // Check if result comes from the correct activity
-        // The next 19 lines of the code are what you need to copy & paste! :)
         if (requestCode == REQUEST_CODE) {
             val response = AuthenticationClient.getResponse(resultCode, intent)
-            loadAlbums(response.accessToken)
-
-            //            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
-            //                System.out.println(":)");
-            //            }
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                loadAlbums(response.accessToken)
+            }
         }
     }
 
-    private fun loginSpotify() {
-        val builder = AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
-        builder.setScopes(arrayOf("user-library-read"))
-        val request = builder.build()
+    fun sync() {
+        loginSpotify()
+    }
+
+    fun loginSpotify() {
+        val request = AuthenticationRequest
+                .Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+                .setScopes(arrayOf("user-library-read")).build()
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request)
+        // callback uses onActivityResult function
+    }
+
+    fun updateDatabase() {
+        // TODO
     }
 
     private fun loadAlbums(token: String) {
         val service = SpotifyService()
         val albums = service.getAlbums(token)
+
+        val fst = albums[0]
+        Util.saveCover(
+                Util.loadBitmap(fst.imageUrl(AlbumModel.ImageSize.MEDIUM)),
+                fst.imageId)
+
+
         val listView = findViewById(R.id.albumListView) as ListView
         listView.adapter = SpotifyAlbumAdapter(this, albums)
-    }
-
-    private fun wer(fstAlbum: AlbumModel) {
-        val ctx = this
-        runBlocking {
-            async {
-                AlbumDatabase.getInstance(ctx)!!.albumDAO().insert(
-                        AlbumEntity(
-                                null,
-                                fstAlbum.name,
-                                fstAlbum.artist,
-                                fstAlbum.imageId))
-            }.await()
-        }
-    }
-
-    private fun q():List<AlbumEntity> {
-        val ctx = this
-        return runBlocking { async {AlbumDatabase.getInstance(ctx)!!.albumDAO().getAll()}.await() }
     }
 }
 
--- app/src/main/java/com/mkolibaba/spotifylibrary/util/Util.kt
+++ app/src/main/java/com/mkolibaba/spotifylibrary/util/Util.kt
@@ -2,15 +2,20 @@ package com.mkolibaba.spotifylibrary.util
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.provider.MediaStore
+import android.os.Environment
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.runBlocking
+import java.io.File
+import java.io.FileOutputStream
 import java.net.URL
 import java.util.*
 
 class Util {
     companion object {
+        private val SPOTIFY_LIBRARY_FOLDER = "SpotifyLibrary"
+        private val COVER_FOLDER = "cover"
+
         fun loadBitmap(url: String): Bitmap {
             val bitmapFuture = async {
                 Picasso.get().load(url).get()
@@ -34,8 +39,26 @@ class Util {
         fun randomString(length: Int): String {
             val chars = "0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
             val rnd = Random()
-            return IntArray(length, { _ -> rnd.nextInt(chars.length) })
-                    .map { chars[it] }.joinToString()
+            return CharArray(length, { _ -> chars[rnd.nextInt(chars.length)] })
+                    .joinToString("")
+        }
+
+        fun saveCover(cover: Bitmap, name: String) {
+            val dirPath = Environment.getExternalStorageDirectory().toString()
+                    .plus("/").plus(SPOTIFY_LIBRARY_FOLDER)
+                    .plus("/").plus(COVER_FOLDER)
+
+            val directory = File(dirPath)
+            if (!directory.mkdirs()) {
+                return
+            }
+
+            val file = File(directory, name.plus(".jpg"))
+            val stream = FileOutputStream(file)
+            stream.use {
+                cover.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+                stream.flush()
+            }
         }
     }
 }
